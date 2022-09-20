e47f32aa3b940af857b2c2b91fe7e846581a8fcb
Sudzuya <79112652+Sudzuya@users.noreply.github.com>
2022-09-20 17:58:20 +0400

    .

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -5,8 +5,6 @@ import { flsModules } from "./modules.js";
 
 import '../files/forms/customRange.js';
 
-import '../libs/simpleParallax.min.js';
-
 let burger = document.querySelector('.header__burger')
 burger.addEventListener('click', ()=> {
     burger.classList.toggle('active')
@@ -84,11 +82,110 @@ let bgTheme = document.querySelector('.bg-theme')
 bgTheme.addEventListener('click', () => 
     document.querySelector('html').classList.add('light'))
 
+// function isHidden(element){
+//     const elementRect = element.getBoundingClientRect();
+//     const elementHidesUp = elementRect.top < 0;
+//     const elementHidesLeft = elementRect.left < 0;
+//     const elementHidesDown = elementRect.bottom > window.innerHeight;
+//     const elementHidesRight = elementRect.right > window.innerWidth;
+//     const elementHides = elementHidesUp || elementHidesLeft || elementHidesDown || elementHidesRight;
+//     console.log(elementHides);
+//     return elementHides;
+// }
 
-const image = document.getElementsByClassName('all-game__bg');
-new simpleParallax(image, {
-    delay: 0,
-    orientation: 'down',
-    scale: 1.5,
-});
-    
+    // isHidden(scrollBg) 
+    function countHidden(element){
+        const elementRect = element.getBoundingClientRect();
+        const elementHides = { 
+            up: Math.max(0,0 - elementRect.top),
+            left: Math.max(0,0 - elementRect.left),
+            down: Math.max(0,elementRect.bottom - window.innerHeight),
+            right: Math.max(0,elementRect.right - window.innerWidth)
+        };
+        // console.log(elementHides.up);
+        return elementHides;
+    }
+
+
+let scrollBg = document.querySelector('.scroll-bg');
+    // scrollBg.querySelectorAll('.')
+    if (scrollBg.classList.contains('._watcher-view')) {
+        console.log(scrollBg);
+    }
+
+
+
+var observeObject = function () {
+    var _class = {
+      init: function (selector, callback) {
+        var element = document.querySelector(selector);
+  
+        try {
+          var observer = new MutationObserver(function (mutations) {
+              mutations.forEach(function (mutation) {
+                callback(mutation.target, mutation.attributeName, mutation.oldValue);
+              });		
+            });
+  
+          observer.observe(element, {
+            attributes: true,
+            subtree: true,
+            attributeOldValue: true
+          });
+        } catch (z) {
+          element.addEventListener('DOMAttrModified', function (e) {
+            callback(e.target, e.attrName, e.prevValue);
+          }, false);
+        }
+      }
+    };
+  
+    return _class;
+}();
+
+var observeTest = function () {
+    observeObject.init('.scroll-bg', function (target, name, oldValue) {
+
+        console.log(target);
+
+      if (target.classList.contains('_watcher-view')) {
+        
+        window.addEventListener('scroll', () => {
+            let value = window.scrollY / window.scrollY
+            let value2 = window.scrollY
+            let value3 = value / value2 * 10000
+            
+            let upScroll = countHidden(scrollBg)
+
+            // console.log(upScroll.up);
+
+            let item3 = target.querySelector('[data-scroll="3"]')
+            let item4 = target.querySelector('[data-scroll="4"]')
+            let item5 = target.querySelector('[data-scroll="5"]')
+            let item6 = target.querySelector('[data-scroll="6"]')
+            let item7 = target.querySelector('[data-scroll="7"]')
+            let item8 = target.querySelector('[data-scroll="8"]')
+            let item9 = target.querySelector('[data-scroll="9"]')
+            let item10 = target.querySelector('[data-scroll="10"]')
+            let item11 = target.querySelector('[data-scroll="11"]')
+            let item12 = target.querySelector('[data-scroll="12"]')
+
+
+            item3.style.transform = `translate(0, -${value3 * 12 + 'px'})`
+            item4.style.transform = `translate(0, -${value3 * 24 + 'px'})`
+            item5.style.transform = `translate(0, -${value3 * 36 + 'px'})`
+            item6.style.transform = `translate(0, -${value3 * 41 + 'px'})`
+            item7.style.transform = `translate(0, -${value3 * 50 + 'px'})`
+            item8.style.transform = `translate(0, -${value3 * 12 + 'px'})`
+            item9.style.transform = `translate(0, -${value3 * -27 * 0.2 + 'px'})`
+            item10.style.transform = `translate(0, -${value3 * -36 * 0.4 + 'px'})`
+            item11.style.transform = `translate(0, -${value3 * -41 * 0.6 + 'px'})`
+            item12.style.transform = `translate(0, -${value3 * -50 * 0.8 + 'px'})`
+
+        })
+
+      } else {
+        false
+      }
+    });
+  } ();
